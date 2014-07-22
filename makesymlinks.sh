edfead94e20c51f389cfd46216d1c1b9cfad93b7
Andrei Vacariu <andrei@avacariu.me>
2014-07-22 16:07:06 -0700

    Add byobu config.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,7 +2,7 @@
 
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="bashrc vimrc vim gitconfig"
+files="bashrc vimrc vim gitconfig byobu"
 
 echo "Creating $olddir to backup any existing dotfiles in -"
 mkdir -p $olddir
