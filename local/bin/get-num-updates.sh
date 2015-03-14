783c18e00fcfc80c292772ae823ad8928a81e2cb
Andrei Vacariu <andrei@avacariu.me>
2015-03-14 13:40:57 -0700

    Speed up apt-check for tmux
    It now makes sure there's only one apt-check running at any one time and it'll
    only check every 30 seconds.

---
+++ local/bin/get-num-updates.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILE=/tmp/NUM_APT_UPDATES_SOME_RANDOM_TEXT
+LOCK=$FILE-lock
+
+# first we just want to just return the value we computed before
+if [ -e $FILE ]; then
+    cat $FILE
+fi
+
+# afterwards, we want to update the value in the file
+if ! [ -e $LOCK ]; then
+    # grab the lock in the main thread to minimize race conditions
+    touch $LOCK
+    {
+        /usr/lib/update-notifier/apt-check 2>&1 | sed 's/;.*//' | sed 's/0//' > $FILE
+
+        # make sure we keep the lock for another 30 seconds so that we don't check
+        # the number of updates more often than every 30 seconds
+        sleep 30
+        rm -f $LOCK
+    } &
+fi
