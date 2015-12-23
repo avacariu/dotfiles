fce7934e262faea06d1cce00589abe7f27dca25e
Andrei Vacariu <andrei@avacariu.me>
2015-12-23 15:29:15 -0800

    Update powerline URL

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -32,7 +32,8 @@ cp $dir/local/bin/get-num-updates.sh ~/.local/bin/
 
 echo "Setting up powerline fonts"
 cd /tmp
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
 fc-cache -vf ~/.fonts
 mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
