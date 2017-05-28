91cabab2f65a9c007ede8f6ce52e1ca2f2fe1983
Andrei Vacariu <andrei@avacariu.me>
2017-05-28 11:01:35 -0700

    Model i3lock.sh based on i3lock manpage

--- i3lock.sh
+++ i3lock.sh
@@ -2,12 +2,12 @@
 
 OLD_TIMEOUT=$(xset q | grep --perl-regexp -o 'Standby: \K\d+')
 
-# XXX This is a workaround for xset in i3/config not setting DPMS timeouts
-# properly
-if [[ $OLD_TIMEOUT -eq 0 ]]; then
-    OLD_TIMEOUT=300
-fi
+revert() {
+    xset dpms $OLD_TIMEOUT $OLD_TIMEOUT $OLD_TIMEOUT
+}
 
-xset dpms 30 30 30
+trap revert HUP INT TERM
+
+xset +dpms dpms 30 30 30
 i3lock -i $HOME/Pictures/wallpapers/Black_Diamonds.png -t -n
-xset dpms $OLD_TIMEOUT $OLD_TIMEOUT $OLD_TIMEOUT
+revert
