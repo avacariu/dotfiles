a1211e8abd65dd4f0ca41f679c4896dce584307c
Andrei Vacariu <andrei@avacariu.me>
2017-05-28 10:24:42 -0700

    Set a DPMS timeout in i3lock.sh

--- i3lock.sh
+++ i3lock.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# NOTE: That `xset` won't turn off the screen if this script is run by
-# xautolock! You'll need to use xset with a delay in the i3 config
+OLD_TIMEOUT=$(xset q | grep --perl-regexp -o 'Standby: \K\d+')
 
-i3lock -i $HOME/Pictures/wallpapers/Black_Diamonds.png -t
-sleep 30
-pgrep i3lock && xset dpms force off
+xset dpms 30 30 30
+i3lock -i $HOME/Pictures/wallpapers/Black_Diamonds.png -t -n
+xset dpms $OLD_TIMEOUT $OLD_TIMEOUT $OLD_TIMEOUT
