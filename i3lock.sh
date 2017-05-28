2dab09e1962e92526f232bfca429b07cad1b70b4
Andrei Vacariu <andrei@avacariu.me>
2017-05-27 19:28:11 -0700

    Set up autolocking in i3

---
+++ i3lock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# NOTE: That `xset` won't turn off the screen if this script is run by
+# xautolock! You'll need to use xset with a delay in the i3 config
+
+i3lock -i $HOME/Pictures/wallpapers/Black_Diamonds.png -t
+sleep 30; pgrep i3lock && xset dpms force off
