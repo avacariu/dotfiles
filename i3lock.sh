4be61747a27a7c245c5907fc9230de25c912201f
Andrei Vacariu <andrei@avacariu.me>
2017-05-28 10:41:56 -0700

    Add workaround for xset not working in i3/config

--- i3lock.sh
+++ i3lock.sh
@@ -2,6 +2,12 @@
 
 OLD_TIMEOUT=$(xset q | grep --perl-regexp -o 'Standby: \K\d+')
 
+# XXX This is a workaround for xset in i3/config not setting DPMS timeouts
+# properly
+if [[ $OLD_TIMEOUT -eq 0 ]]; then
+    OLD_TIMEOUT=300
+fi
+
 xset dpms 30 30 30
 i3lock -i $HOME/Pictures/wallpapers/Black_Diamonds.png -t -n
 xset dpms $OLD_TIMEOUT $OLD_TIMEOUT $OLD_TIMEOUT
