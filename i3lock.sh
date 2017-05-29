c4d1779482ef1288e3416a9592c6bc09efbe510d
Andrei Vacariu <andrei@avacariu.me>
2017-05-29 13:50:06 -0700

    Lock screen immediately when using CTRL-ALT-L

--- i3lock.sh
+++ i3lock.sh
@@ -9,14 +9,22 @@ revert() {
 trap revert HUP INT TERM
 
 xset +dpms
+
+# NOTE: Setting timeouts has to be before the 'force off' because otherwise the
+# screen will turn back on. It still turns back on sometimes, but this makes it
+# less likely to do so.
+xset dpms 30 30 30
 xset dpms force off
 
-sleep 5
+if [[ $1 != "immediate" ]]; then
+    # Wait 5 seconds before locking the screen in case the mouse got moved
+    sleep 5
 
-if xset q | grep --quiet '^  Monitor is On'; then
-    exit
+    if xset q | grep --quiet '^  Monitor is On'; then
+	revert
+	exit
+    fi
 fi
 
-xset dpms 30 30 30
 i3lock -i $HOME/Pictures/wallpapers/Black_Diamonds.png -t -n
 revert
